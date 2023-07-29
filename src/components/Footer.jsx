3710afa3f1fe17cb37d5b2022bcae9a6ce433316
bentwhiskers <abbott1414@gmail.com>
2023-07-29 11:35:06 -0400

    started code structure for each component and to App.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'; 
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className=''>
+                Other Links
+            </div>
+        </footer>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import Nav from './Nav'
+
+export default function Header() {
+    return (
+        <header>
+            <h1>
+                Erica Covington-Abbott
+            </h1>
+            <Nav />
+        </header>
+    )
+}
+
