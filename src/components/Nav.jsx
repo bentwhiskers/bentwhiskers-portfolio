c457329721153def5e18c16c8a780b9b32c2c6f5
bentwhiskers <abbott1414@gmail.com>
2023-07-31 20:43:12 -0400

    styled nav tabs under app.css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,39 +3,41 @@ import { Link, useLocation } from 'react-router-dom';
 export default function Nav() {
   const currentURL = useLocation().pathname;
   return (
-  <nav id='nav'>   
+  <nav id='nav' className='container-fluid'>   
   <ul className="nav justify-content-end nav-tabs" id='mytabs'>
-    <div className='tab-pane'>
-      <li className="nav-item" id='L1'>
-        <Link to="/" className={currentURL === "/" ? "nav-link active" : "nav-link"} aria-current="page">Erica Covington-Abbott</Link>
-      </li>
-    </div>
-    <div className='tab-pane'>
-      <li className="nav-item" id='L2'>
-        <Link to="/about" className={
+    <div className='row'>
+      <div className='tab-pane col-sm-auto'>
+        <li className="nav-item" id='L1'>
+          <Link to="/" className={currentURL === "/" ? "nav-link active" : "nav-link"} aria-current="page">Erica Covington-Abbott</Link>
+        </li>
+      </div>
+      <div className='tab-pane col-sm-auto'>
+        <li className="nav-item" id='L2'>
+          <Link to="/about" className={
                       currentURL === "/about" ? "nav-link active" : "nav-link"
                     }>
                       About Me
-        </Link>
-      </li>
-    </div>
-    <div className='tab-pane'>
-      <li className="nav-item" id='L3'>
-        <Link to="/portfolio" className={
+          </Link>
+        </li>
+      </div>
+      <div className='tab-pane col-sm-auto'>
+        <li className="nav-item" id='L3'>
+          <Link to="/portfolio" className={
                       currentURL === "/portfolio" ? "nav-link active" : "nav-link"
                     }>
                       Portfolio
-        </Link>
-      </li>
-    </div>
-    <div className='tab-pane'>
-      <li className="nav-item" id='L4'>
-        <Link to="/resume" className={
+          </Link>
+        </li>
+      </div>
+      <div className='tab-pane col-sm-auto'>
+        <li className="nav-item" id='L4'>
+          <Link to="/resume" className={
                       currentURL === "/resume" ? "nav-link active" : "nav-link"
                     }>
                       Resume
-        </Link>
-      </li>
+          </Link>
+        </li>
+      </div>
     </div>
   </ul>
   </nav>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,6 +2,6 @@ import fakeResume from '../images/fakeResume.png';
 
 export default function Resume() {
     return (
-        <img id='resume' className='' src={fakeResume} alt="pic of an example resume"></img>
+        <img id='resume' className='img-fluid' src={fakeResume} alt="pic of an example resume"></img>
     )
 }
